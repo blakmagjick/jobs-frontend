5a1e9ceafa408b3b0e6be2a3403cd8a745740edd
Suzy Q <blakmagjick@users.noreply.github.com>
2022-02-24 21:17:05 -0800

    Added getJobs function. Fetch working.

--- src/App.js
+++ src/App.js
@@ -6,10 +6,35 @@ export default class App extends Component {
   constructor() {
     super()
     this.state = {
-
+      baseURL: 'http://localhost:3003',
+      jobs: []
     }
   }
+
+  getJobs = () => {
+    fetch(this.state.baseURL + '/jobs/')
+    .then (response => {
+      console.log(response)
+      if (response.status === 200) {
+        return response.json() 
+      } else {
+        return []
+      }
+    })
+    .then(body => {
+      console.log(body)
+      this.setState({
+        // jobs: body
+      })
+    })
+  }
+
+  componentDidMount() {
+    this.getJobs()
+  }
+
   render() {
+    // console.log(this.state.jobs)
     return (
       <div className="App">
         <header className="App-header">
@@ -19,18 +44,4 @@ export default class App extends Component {
       </div>
     )
   }
-
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <h1>Job Tracker</h1>
-//       </header>
-//       <JobApps />
-//     </div>
-//   );
-// }
-
-// export default App;
