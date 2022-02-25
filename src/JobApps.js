3b0c1d268bdcb262a9ffddf7a0094b3a83db03d5
Suzy Q <blakmagjick@users.noreply.github.com>
2022-02-24 21:33:41 -0800

    Added title and notes to each job listed

--- src/JobApps.js
+++ src/JobApps.js
@@ -1,12 +1,17 @@
 import React from 'react';
+// import './App.css';
 
 export default function JobApps (props) {
     return (
         <React.Fragment>
-            <h3>Jobs</h3>
+            <h3>Applied to Jobs</h3>
             {props.jobs.map((job, i) => {
                 return (
-                    <p>{job.companyName}</p>
+                    <div key={i}>
+                    <p>Company Name: {job.companyName} <br />
+                    Job Title: {job.jobTitle} <br />
+                    Notes: {job.notes}</p>
+                    </div>
                 )
             })}
         </React.Fragment>
